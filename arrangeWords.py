e7e95adc389bf8b8bdb0bd3f9122971be883200c
iculus <mgsoroka@gmail.com>
2017-09-19 15:38:26 -0700

    output of arragneWords works as expected

--- arrangeWords.py
+++ arrangeWords.py
@@ -1,36 +1,69 @@
 def splitUpParagraph(workingInstruction, start, lineLength):
 	Working = True
+	signature = '^'
+	#first break up the instruction at line breaks defined by the user
+	firstPassList = []
+	tempParagraph = []
+	#since the prgram uses a signature character, check that this char is not in use
+	#send an error if it is!
+	for letter in workingInstruction:
+		if letter == signature:
+			print 'error: do not use the', signature, \
+			'character in the instruction or change the', \
+			'signature character in splitUpParagraph'
+			quit()
+	for letter in workingInstruction:
+		if letter != '\n':
+			tempParagraph.append(letter)
+			
+		if letter == '\n':
+			#add an ending signature
+			tempParagraph.append(signature)
+			thisParagraph = ''.join(tempParagraph)
+			firstPassList.append(thisParagraph)
+			tempParagraph = []
+	#at this point the variable firstPassList has the user line break formatting
+	#there is one list entry per user paragraph
+			
+	#with firstPassList populated, for each list item check for line lengh requirements
 	newLines = []
-	for i in range (0,20):
-		spaces = []
-		chars = []
-		for position,character in enumerate(workingInstruction[start:lineLength+start]):
-			if character == ' ' or character == '\n':
-				spaces.append(position+start)
-				chars.append(character)
-		newlineFound = False
-		for location, newline in enumerate(chars):
-			if newline == '\n':
-				newLineFound = True
-		if newLineFound == True:
-			end = spaces[location]
-		elif newLineFound == False:
+	lineLengthHere = 0
+	exitCondition = False
+	for paragraph in firstPassList:
+		newLinesThisParagraph = []
+		start = 0
+		if len(paragraph) <= lineLength: lineLengthHere = len(paragraph) 
+		if len(paragraph) > lineLength: lineLengthHere = lineLength
+		for i in range (0,100):
+			initialChunk = paragraph[start:lineLengthHere+start]
+			spaces = []
+			chars = []
+			for position, char in enumerate(initialChunk):
+				if char == ' ' or char == signature:
+					spaces.append(position+start)
+					chars.append(char)
+					if char == signature:
+						exitCondition = True
 			end = spaces[-1]
-		print spaces, chars, newLineFound
-		newLines.append(workingInstruction[start:end])
-		try: start = end+1
-		except: start = end
+			finalChunk = paragraph[start:end]
+			if finalChunk[0:1] == ' ':
+				finalChunk = finalChunk[1:]
+			start = end
+			if len(finalChunk) > 0:
+				newLinesThisParagraph.append(finalChunk)
+		newLines.append(newLinesThisParagraph)
+			
+		
+	
 	return newLines
 
-
 if __name__ == "__main__":
-	sentenceFile = open('sentence.mike')
+	sentenceFile = open('instructions/sentence.mike')
 
 	instructions = []
 	for instruction in sentenceFile:
 		instructions.append(instruction)
 	
-	#print instructions
 	thisSentence = ''.join(instructions)
 	print thisSentence
 
@@ -38,4 +71,5 @@ if __name__ == "__main__":
 
 	for i in smallerWords:
 		print i
+		pass
 
